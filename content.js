54a479ce1b53d0a070dbf5ac367b9defa4b79d46
Calin Biberea <calinbiberea@gmail.com>
2021-01-23 20:51:16 +0200

    Add tweet support for low toxicty

--- content.js
+++ content.js
@@ -1,17 +1,17 @@
 // content.js
 function injectAlertIfRootAvailable() {
-    var root = document.querySelector('[aria-label="Timeline: Your Home Timeline"]')
+    var root = document.querySelector('[aria-labelledby="accessible-list-0"]')
     if (!root) {
         // The node we need does not exist yet.
         // Wait 500ms and try again
         window.setTimeout(injectAlertIfRootAvailable, 500);
         return;
     }
-    chrome.runtime.sendMessage({message: "injectAlert"}, function (response) {
-    });
+    if (root) {
+        chrome.runtime.sendMessage({message: "injectAlert"}, function (response) {
+        });
+    }
 }
 
 injectAlertIfRootAvailable();
-chrome.tabs.onUpdated.addListener(function () {
-    injectAlertIfRootAvailable();
-});
+
